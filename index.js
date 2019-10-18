20bc542082355abf9b948f83c0322ad87cdf3148
IrfSuar <iasl96@msn.com>
2019-10-17 20:59:39 -0500

    second try

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 require('dotenv').config()
 
-// AQUÍ SE LEVANTA LA CONEXIÓN A BASE DE DATOS
-// Primero se levanta la bdd para poder asegurar que tendremos datos con que trabajar.
-//La 
 const dbConnect = require('./src/lib/db')
 const server = require('./src/server')
 const listenServer = function() {
